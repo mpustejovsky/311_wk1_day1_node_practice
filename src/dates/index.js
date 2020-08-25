62d3eb90ef758b6acd989e97233d1925ce5ed092
mpustejovsky <mpustejovsky@yahoo.com>
2020-08-24 19:32:29 -0500

    finished product

--- src/dates/index.js
+++ src/dates/index.js
@@ -3,15 +3,55 @@
 const today = () => {
   // write code for dates.today
 
+
+  let d = new Date();
+  let weekday = [];
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  let n = weekday[d.getDay()];
+  return n;
+
 }
 
 const calendar = () => {
   // write code for dates.calendar
+  let day = new Date();
+  let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  let monthRet = months[day.getMonth()];
+  let dayRet = day.getDate();
+  let yearRet = day.getFullYear();
+
+  return `${monthRet} ${dayRet}, ${yearRet}`
+
+
 
 }
 
 const currentTime = () => {
   // write code for dates.currentTime
+  let day = new Date();
+  let hours = day.getHours();
+  let amPM = 'AM'
+  if (hours > 11) {
+    amPM = 'PM'
+    if (hours > 12) { hours = hours - 12 }
+  }
+
+  let minutes = day.getMinutes();
+  let seconds = day.getSeconds();
+
+  hours = String('0' + hours).slice(-2);
+  minutes = String('0' + minutes).slice(-2);
+  seconds = String('0' + seconds).slice(-2);
+
+  return `${hours}:${minutes}:${seconds} ${amPM}`
+
 
 }
 
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,15 +1,33 @@
 const split = (str, delim) => {
   // write code for strings.split
 
+  return str.split("-")
+
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
+  let pairArr = str.split('');
 
+  let pairArr2 = []
+  let counter = 0;
+  for (let i = 0; i < (str.length); i++) {
+
+    if (i % 2 == 0) {
+      pairArr2[counter] = pairArr[i] + pairArr[i + 1]
+      counter++;
+    }
+  }
+  return pairArr2;
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
+
+  let splitString = str.split('');
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join('');
+  return joinArray;
 
 }
 
